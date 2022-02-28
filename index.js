0ed8ae70cd1801be934011e7179ee75524d97912
valeria y sofia <ramirez.jaramillo.valeria@gmail.com>
2022-02-28 07:43:08 -0500

    solucion taller 1

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("Hola mundo");
+// comentarios en linea
+/*comentario 
+de 
+varias 
+lineas 
+*/
+
+var nombreUsuario; 
+let edadUsuario;
+/* declarar Variables
+*/
+var nombreVariable;
+//crear variable sin asignar valor 
+/*
+inicializar variables:
+asignar un valor a el momento de crear la variable
+*/
+var gradoEstudiante="10A";
+/*asignar un valor:
+igualar una variable existente a un nuevo valor*/
+edadUsuario=16;
+//asignar valor desde otra variable
+console.log(edadUsuario);
+var edad=17
+edadUsuario=edad;
+console.log(edadUsuario)
+/*asignar valor desde el teclado:
+existen dos maneras de hacerlo, la primera es por ventana emergente y la segunda es por medio de un formulario en html
+*/
+var valor= prompt("ingrese un valor ");
+console.log("la variable vale " + valor);
+/*
+constante: son valores que no pueden ser cambiados durante la ejecucion del codigo. las constantes siempre deben ser inicializadas
+*/
+const  PI = 3.1416;
+//imPrimir Por consola
+//simple 
+console.log ("xd");
+console.log (valor);
+//concatenado 
+console.log ("esto vale " + valor );
---
+++ taller 1.js
@@ -0,0 +1,13 @@
+//Archivo de práctica y pruebas de JavaScript.//
+/*
+Taller 1
+Fecha 21/02/2022
+Realizado por: sofi y vale
+*/
+var nombreEstudiante; //string 
+var gradoEstudiante //string
+var edadEstudiantes //int
+gradoEstudiante= "10a"
+nombreEstudiante= "sofia"
+var edadEstudiantes= prompt("ingrese su edad ");
+console.log("El estudiante " + nombreEstudiante +" del grado " + gradoEstudiante + " tiene " + edadEstudiantes +" años") 
